59ca41169c37f7990f4fe632723a914f096242b7
mehak-garg <mehakgarg741@gmail.com>
2026-05-20 17:59:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let userName=document.querySelector("#username");
+let password=document.querySelector("#password");
+let checkbox=document.querySelector("#checkbox");
+let btn=document.querySelector("#submit");
+let existing=document.querySelector("#existing");
+function onload(){
+	
+}
+btn.addEventlistener("click",()=>{
+	if(checkbox.checked){
+		if(userName.value&&password.value)
+		{
+let credential={username:userName.value,password:password.value};
+localStorage.setItem("credentials",JSON.stringify(credential));
+
+		}
+	}
+
+
+
+	
+})
